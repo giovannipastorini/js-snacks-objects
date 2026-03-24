17e167f6089bdd018609dc7d30120387727901b7
giovannipastorini <giovannipastorini9@gmail.com>
2026-03-24 17:30:17 +0100

    snack 2: task 2 completed

--- main.js
+++ main.js
@@ -70,10 +70,13 @@ console.log(bikes[x]);
 
 
 //Snack2
+//TASK 1:
 //Creare un array di oggetti di squadre di calcio.
 //Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 //Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+//TASK 2:
 //Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+//TASK 3:
 //Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
 //TASK 1:
@@ -104,6 +107,24 @@ const soccerTeams=[
         falliSubiti:0
     }    
 ];
+/* console.log(soccerTeams); */
+
+//TASK 2:
+//Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+
+//ciclo for per scorrere tutti gli oggetti dell'array e aggiornare le proprietà
+for(let i=0; i<soccerTeams.length; i++){
+    //assegno un numero random fra 1 e 50
+    soccerTeams[i]["points"]= Math.floor(Math.random() * 50) + 1;
+    //assegno un numero random fra 1 e 10
+    soccerTeams[i]["falliSubiti"]= Math.floor(Math.random() * 10) + 1;
+};
+console.log(soccerTeams);
+
+
+
+
+
 
 
 //define object
